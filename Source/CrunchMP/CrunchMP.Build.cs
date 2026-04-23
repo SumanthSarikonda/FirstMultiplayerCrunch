73f33b462fc8970ef4dacf6b3523a4625e98154d
Sumanth <iamsumanth64@gmail.com>
2026-04-23 18:08:26 +0530

    initial unreal project setup with source code

--- Source/CrunchMP/CrunchMP.Build.cs
+++ Source/CrunchMP/CrunchMP.Build.cs
@@ -10,7 +10,15 @@ public class CrunchMP : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", 
+			                                                         "GameplayAbilities", 
+			                                                         "GameplayTasks", 
+			                                                         "GameplayTags",
+			                                                         "UMG",
+			                                                         "Slate",
+			                                                         "SlateCore",
+			                                                         "AIModule"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
---
+++ Source/CrunchMP/Private/DebugHelper.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+namespace  Debug
+{
+	static void print(const FString& MSG, const FColor Color = FColor::MakeRandomColor(), int32 Inkey = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(Inkey, 7.f, Color, MSG);
+		
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *MSG);
+		}
+	}
+	
+	static void print(const FString& FloatTitle, float FloatValueToPrint, int32 Inkey = -1, const FColor Color = FColor::MakeRandomColor())
+	{
+		if (GEngine)
+		{
+			const FString FinalMSG = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
+			GEngine->AddOnScreenDebugMessage(Inkey, 7.f, Color, FinalMSG);
+			UE_LOG(LogTemp, Warning, TEXT("%s"), *FinalMSG);
+		}
+	}
+}
+
